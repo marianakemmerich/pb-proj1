671c4ed974b80aec6b99b60aa7785cfdb6a5c03b
Mariana Kemmerich <marianarkemmerich@gmail.com>
2024-06-04 02:19:31 -0300

    Added adopt and donate modals

---
+++ app.js
@@ -0,0 +1,36 @@
+const adoptButton = document.querySelector('#adopt')
+const applyAdoptButton = document.querySelector('#apply-adopt')
+const applyToAdoptButton = document.querySelector('#apply-to-adopt')
+const adoptModal = document.querySelector('.adopt-modal')
+const cancelButton = document.querySelector('#cancel')
+const donateButton = document.querySelector('#donate')
+const donateModal = document.querySelector('.donate-modal')
+const cancelDonationButton = document.querySelector('#cancel-donation')
+
+adoptButton.onclick = function() {
+    adoptModal.showModal()
+}
+
+applyAdoptButton.onclick = function() {
+    adoptModal.showModal()
+}
+
+applyToAdoptButton.onclick = function() {
+    adoptModal.showModal()
+}
+
+cancelButton.onclick = function() {
+    adoptModal.close()
+}
+
+donateButton.onclick = function() {
+    donateModal.showModal()
+}
+
+cancelDonationButton.onclick = function() {
+    donateModal.close()
+}
+
+document.getElementById('wanna-adopt').addEventListener('click', function() {
+    window.location.href = './adoptables-page.html'
+})
